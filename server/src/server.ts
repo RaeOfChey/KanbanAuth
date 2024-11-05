732b6f996c0962a92c6e771302b6b014215040e9
RaeofChey <cheyennaraelynn@gmail.com>
2024-11-05 17:37:21 -0600

    Adjusted server.ts for catch-all route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,14 @@
-const forceDatabaseRefresh = false;
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,7 +19,12 @@ app.use(express.static('../client/dist'));
 app.use(express.json());
 app.use(routes);
 
-sequelize.sync({force: forceDatabaseRefresh}).then(() => {
+// Catch-all route to serve React's index.html for unrecognized routes
+app.get('*', (__req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
+
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
