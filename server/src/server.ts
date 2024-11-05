e18914ecd4a0f56e4775a202ed69c9228af52f95
RaeofChey <cheyennaraelynn@gmail.com>
2024-11-05 09:12:16 -0600

    Adjusted package.json

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,18 @@
 const forceDatabaseRefresh = false;
-console.log('JWT Secret Key:', process.env.JWT_SECRET_KEY);
 
 import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-import cors from 'cors'; // Import cors
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Enable CORS for all routes
-app.use(cors());
-
+// Serves static files in the entire client's dist folder
 app.use(express.static('../client/dist'));
+
 app.use(express.json());
 app.use(routes);
 
